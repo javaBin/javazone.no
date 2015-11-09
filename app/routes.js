a4625cd369de67755e61f631edcc234abdd1e933
Bendik Solheim <bendik@solheim.im>
2015-11-09 17:12:18 +0100

    Adds contact component and endpoint

--- app/routes.js
+++ app/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import Root from './components/root';
 import Index from './pages/index';
+import Contact from './pages/contact';
 import NotFound from './pages/404.js';
 
 export default (
     <Route path='/' component={Root}>
         <IndexRoute component={Index} />
+        <Route path='contact' component={Contact} />
         <Route path='*' component={NotFound} />
     </Route>
 );
