902634d731e4576aef8198dcfd12d1f0163dcf82
Bendik Solheim <bendik@solheim.im>
2015-11-23 22:36:16 +0100

    Some small changes

--- app/pages/index.js
+++ app/pages/index.js
@@ -10,7 +10,11 @@ export default () => (
         <div className='page__header' style={headerStyle}>
             <h1 className="page__title teaser__title">JavaZone 2016</h1>
         </div>
-        <h2 className='teaser__dates'>Oslo Spektrum, 7th – 8th September, 2016</h2>
-        <h2 className='teaser__punchline'>Save the date.</h2>
+        <div className='textblock'>
+            <div className='textblock--text'>
+                <h2 className='teaser__dates'>Oslo Spektrum, 7th – 8th September, 2016</h2>
+                <h2 className='teaser__punchline'>Save the date.</h2>
+            </div>
+        </div>
     </div>
 );
