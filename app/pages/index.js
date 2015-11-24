e42f2d06833f3fa3ab12fa09742c1d6bd3da720f
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2015-11-24 19:09:12 +0100

    Moar twerks

--- app/pages/index.js
+++ app/pages/index.js
@@ -14,9 +14,10 @@ export default () => (
             <div className='textblock--centered__text'>
 		<h2 className='page__sub-title'>Oslo Spektrum<br />September 7–8th 2016</h2>
                 <p className='page__paragraph'>
-		    Join us in 2016 celebrating the 15th year of JavaZone. The largest community driven conference for developers.
-		    The date is now set: JavaZone 2016 will take place at <br />Oslo Spektrum, 7th and 8th of September.<br /><br />
-		    We are working hard to make JavaZone 2016 the most epic JavaZone ever.<br />Stay tuned for more info.
+                    Join us in 2016 celebrating the 15th year of JavaZone. The largest community driven conference for developers.
+                    The date is now set: JavaZone 2016 will take place at <br />Oslo Spektrum, 7th and 8th of September.<br /><br />
+                    We are working hard to make JavaZone 2016 the most epic JavaZone ever.<br />Call for Speakers and ticket sales
+                    will start in February, until then <a href="https://twitter.com/JavaZone" className="page__link">stay tuned</a>.
                 </p>
             </div>
         </div>
