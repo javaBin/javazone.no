8dd62c3738d793489255c748d6f9d3aae0f372a9
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2015-11-24 18:37:58 +0100

    Moar tweaks

--- app/pages/index.js
+++ app/pages/index.js
@@ -8,14 +8,15 @@ const headerStyle = {
 export default () => (
     <div className='teaser page'>
         <div className='page__header' style={headerStyle}>
-            <h1 className="page__title teaser__title">JavaZone 2016</h1>
+	    <h1 className="page__title teaser__title">15 Years of JavaZone</h1>
         </div>
         <div className='textblock--centered'>
             <div className='textblock--centered__text'>
-                <h2 className='page__sub-title'>Oslo Spektrum, 7th – 8th September, 2016</h2>
+		<h2 className='page__sub-title'>Oslo Spektrum<br />September 7–8th 2016</h2>
                 <p className='page__paragraph'>
-                    The dates have been set. JavaZone 2016 will take place at Oslo Spektrum, 7th and 8th of September.
-                    We are working hard to make JavaZone 2016 the most epic JavaZone ever – stay tuned for more info.
+		    Join us in 2016 celebrating the 15th year of JavaZone. The largest community driven conference for developers.
+		    The date is now set: JavaZone 2016 will take place at <br />Oslo Spektrum, 7th and 8th of September.<br /><br />
+		    We are working hard to make JavaZone 2016 the most epic JavaZone ever.<br />Stay tuned for more info.
                 </p>
             </div>
         </div>
