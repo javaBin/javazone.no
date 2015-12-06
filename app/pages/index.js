132fa758b7ac31e53bfc96d57631159ec11a1af7
Bendik Solheim <bendik@solheim.im>
2015-12-06 15:38:57 +0100

    Correct spacing on front page

--- app/pages/index.js
+++ app/pages/index.js
@@ -8,11 +8,11 @@ const headerStyle = {
 export default () => (
     <div className='teaser page'>
         <div className='page__header' style={headerStyle}>
-	    <h1 className="page__title teaser__title">15 Years of JavaZone</h1>
+	       <h1 className="page__title teaser__title">15 Years of JavaZone</h1>
         </div>
-        <div className='textblock--centered'>
+        <div className='textblock textblock--centered'>
             <div className='textblock--centered__text'>
-		<h2 className='page__sub-title'>Oslo Spektrum<br />September 7–8th 2016</h2>
+		<h2 className='textblock__title'>Oslo Spektrum<br />September 7–8th 2016</h2>
                 <p className='page__paragraph'>
                     Join us in 2016 celebrating the 15th year of JavaZone. The largest community driven conference for developers.
                     The date is now set: JavaZone 2016 will take place at <br />Oslo Spektrum, 7th and 8th of September.<br /><br />
