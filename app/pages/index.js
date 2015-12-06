54e599ee84005ec4b74236e7ec787a8461dd61ea
Bendik Solheim <bendik@solheim.im>
2015-12-06 20:04:42 +0100

    Fix wrongly left justified text

--- app/pages/index.js
+++ app/pages/index.js
@@ -12,8 +12,8 @@ export default () => (
         </div>
         <div className='textblock textblock--centered'>
             <div className='textblock--centered__text'>
-		<h2 className='textblock__title'>Oslo Spektrum<br />September 7–8th 2016</h2>
-                <p className='page__paragraph'>
+		        <h2 className='textblock__title'>Oslo Spektrum<br />September 7–8th 2016</h2>
+                <p>
                     Join us in 2016 celebrating the 15th year of JavaZone. The largest community driven conference for developers.
                     The date is now set: JavaZone 2016 will take place at <br />Oslo Spektrum, 7th and 8th of September.<br /><br />
                     We are working hard to make JavaZone 2016 the most epic JavaZone ever.<br />Call for Speakers and ticket sales
