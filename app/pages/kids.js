6cfb2e5479c488dc9ea39d95928a3263b3055213
Bendik Solheim <bendik@solheim.im>
2016-01-05 20:42:30 +0100

    JavaZone Kids page no longer breaks horribly

--- app/pages/kids.js
+++ app/pages/kids.js
@@ -6,8 +6,17 @@ const headerStyle = {
 
 export default () => (
     <div className='page kids'>
-        <div class='page__header page__header-centered' style={headerStyle}>
+        <div className='page__header page__header-centered' style={headerStyle}>
             <h1 className='page__title'>JavaZone Kids</h1>
+        </div>
+
+        <div className='textblock textblock--centered'>
+            <div className='textblock--centered__text'>
+                <h2 className='textblock__title'>A taste of JavaZone for young, aspiring developers</h2>
+                <p>
+                    Kids
+                </p>
+            </div>
         </div>
     </div>
 );
