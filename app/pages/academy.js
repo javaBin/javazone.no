decbbeb9e2fb267a5c3b8b3ad88ee1e1a1524fe6
Bendik Solheim <bendik@solheim.im>
2016-01-05 21:46:53 +0100

    Start of academy page styling

--- app/pages/academy.js
+++ app/pages/academy.js
@@ -1,4 +1,7 @@
 import header from '../assets/academy.jpg';
+import teknologihuset from '../assets/academy/teknologihuset.jpg';
+import kvarteret from '../assets/academy/kvarteret.jpg';
+import samfundet from '../assets/academy/samfundet.jpg';
 
 const headerStyle = {
     backgroundImage: `url('${header}')`
@@ -26,20 +29,26 @@ export default () => (
         </div>
 
         <ul className='academy__locations'>
-            <li className='academy__location'>
-                <h3>Oslo</h3>
-                <p>February <strong>16th</strong></p>
-                <p>@ <a href="http://www.teknologihuset.no/">Teknologihuset</a><br/>with 120 students</p>
+            <li className='academy__location academy__location--oslo location'>
+                <div className='location__image' style={{backgroundImage: `url('${teknologihuset}')`}}></div>
+                <h3 className='location__title'>Oslo</h3>
+                <p className='location__date'><strong>February 16th</strong></p>
+                <p className='location__location'>@ <a href="http://www.teknologihuset.no/">Teknologihuset</a></p>
+                <p className='location__pax'>120 students</p>
             </li>
-            <li className='academy__location'>
-                <h3>Bergen</h3>
-                <p>February <strong>17th</strong></p>
-                <p>@ <a href="http://kvarteret.no/">Det Akademiske Kvarter</a><br/>with 120 students</p>
+            <li className='academy__location academy__location--bergen location'>
+                <div className='location__image' style={{backgroundImage: `url('${kvarteret}')`}}></div>
+                <h3 className='location__title'>Bergen</h3>
+                <p className='location__date'><strong>February 17th</strong></p>
+                <p className='location__location'>@ <a href="http://kvarteret.no/">Det Akademiske Kvarter</a></p>
+                <p className='location__pax'>120 students</p>
             </li>
-            <li className='academy__location'>
-                <h3>Trondheim</h3>
-                <p>February <strong>18th</strong></p>
-                <p>@ <a href="https://www.samfundet.no/">Studentersamfundet</a><br/>with 200 students</p>
+            <li className='academy__location academy__location--trondheim location'>
+                <div className='location__image' style={{backgroundImage: `url('${samfundet}')`}}></div>
+                <h3 className='location__title'>Trondheim</h3>
+                <p className='location__date'><strong>February 18th</strong></p>
+                <p className='location__location'>@ <a href="https://www.samfundet.no/">Studentersamfundet</a></p>
+                <p className='location__pax'>200 students</p>
             </li>
         </ul>
         <div className='textblock textblock--centered'>
@@ -54,7 +63,7 @@ export default () => (
             <div className='textblock--centered__text'>
                 <h2 className='textblock__title'>Signup</h2>
                 <p>
-                    <a href="https://twitter.com/JavaZone" className="page__link">stay tuned</a> for information on how to sign up for JavaZone Academy.
+                    <a href="https://twitter.com/JavaZone" className="page__link">Stay tuned</a> for information on how to sign up for JavaZone Academy.
                 </p>
             </div>
         </div>
