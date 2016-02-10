1f630784508ce15f9ddcbe68c124edcd0f2fc675
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2016-02-11 00:11:11 +0100

    Billettpriser, info om moms + litt diverse andre småfikser på tickets-siden

--- app/pages/tickets.js
+++ app/pages/tickets.js
@@ -15,9 +15,9 @@ export default () => (
         <div className='textblock textblock--centered'>
             <div className='textblock__column textblock__column--centered'>
                 <div className='textblock__text textblock__text--fill'>
-                    <h2 className='textblock__title'>Join us for the 15th JavaZone, <br />buy your ticket today!</h2>
+                    <h2 className='textblock__title'>Join us for the 15th JavaZone, <br /><a href="https://www.eventsystems.no/es/event/javazone2016">buy your ticket today</a></h2>
                     <P>
-                        The tickets for JavaZone 2016 are availiable to an extra special Early Bird price until April 1st 2016. Buy your ticket now and save more than 10%. What’s not to like?
+                        The tickets for JavaZone 2016 are availiable to an extra special Early Bird price until April&nbsp;1st&nbsp;2016. Buy your ticket now and save more than 10%. What’s not to like?
                     </P>
                 </div>
             </div>
@@ -27,15 +27,17 @@ export default () => (
             <li className='tickets__type'>
                 <a href='https://www.eventsystems.no/es/event/javazone2016' className='tickets__link'>
                     <div><span className='tickets__icon icon-tag'></span></div>
-                    <div className='tickets__title'>Early bird ticket</div>
-                    <div className='tickets__price'>xxxx NOK</div>
+                    <div className='tickets__title'>Early Bird Ticket</div>
+                    <div className='tickets__price'>5&nbsp;990 NOK</div>
+                    <div className='tickets__vat'>plus VAT</div>
                 </a>
             </li>
             <li className='tickets__type'>
                 <a href='https://www.eventsystems.no/es/event/javazone2016' className='tickets__link'>
                     <div><span className='tickets__icon icon-tag'></span></div>
-                    <div className='tickets__title'>javaBin ticket</div>
-                    <div className='tickets__price'>xxxx NOK</div>
+                    <div className='tickets__title'>javaBin Early Bird</div>
+                    <div className='tickets__price'>4&nbsp;990 NOK</div>
+                    <div className='tickets__vat'>plus VAT</div>
                 </a>
             </li>
         </ul>
@@ -53,12 +55,12 @@ export default () => (
                 </dl>
             </Column>
             <Column>
-                <ColumnHeading>javaBin Member price</ColumnHeading>
+                <ColumnHeading>About the javaBin tickets</ColumnHeading>
                 <P justify={true}>
                     If you are a member of javaBin, you can buy your ticket to a reduced price. Both ticket types gives full access to the conference, but javaBin-tickets are discounted by NOK 1000,–
                 </P>
                 <P justify={true}>
-                    We fully recommend that you consider a membership before buying your ticket. Anyone is welcome to join javaBin, we already have more than 2.000 members from all over the country.
+                    We fully recommend that you consider a membership before buying your ticket. Anyone is welcome to join javaBin, we have more than 2.000 members from all over the country.
                 </P>
                 <P justify={true}>
                     You can read more and buy your membership at <a href="http://java.no/index.html?page=medlemskap">java.no</a>.
