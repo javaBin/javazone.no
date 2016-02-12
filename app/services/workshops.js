fd2899909b92ced731506c8fab9ebcac57813997
Bendik Solheim <bendik@solheim.im>
2016-02-12 11:35:04 +0100

    Use https instead of http

--- app/services/workshops.js
+++ app/services/workshops.js
@@ -1,5 +1,5 @@
 import request from 'superagent';
 
 export function get() {
-    return request.get('http://javazone.no/moosehead/data/workshopList');
+    return request.get('https://javazone.no/moosehead/data/workshopList');
 }
