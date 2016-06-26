545ff0a130acdc486913a9c5f0c845c705ec25ba
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2016-06-26 14:21:57 +0200

    Ny nettside for JavaZone Kids. Fikk ikke helt til å gjenbruke det fine "map'et" øverst på siden som definerte sesjonene siden moosehead ikke er aktivt, så jeg hardkodet ting isteden. Bendik, du får komme og rydde opp etter meg når vi skal koble det på moosehead igjen :)

--- app/components/nav.js
+++ app/components/nav.js
@@ -77,7 +77,7 @@ const Nav = React.createClass({
                         <MenuItem to={'/program'} title='Program' click={this.toggleMenu} />
                         <MenuItem to={'/info'} title='Info' click={this.toggleMenu} />
                         <MenuItem to={'/tickets'} title='Tickets' click={this.toggleMenu} />
-                        <MenuItem to={'/speakers'} title='Speakers' click={this.toggleMenu} />
+                        <MenuItem to={'/kids'} title='Kids' click={this.toggleMenu} />
                         <MenuItem to={'/journeyzone'} title='JourneyZone' click={this.toggleMenu} />
                         <MenuItem to={'/partners'} title='Partners' click={this.toggleMenu} />
                         <MenuItem to={'/about'} title='About' click={this.toggleMenu} />
--- app/pages/kids.js
+++ app/pages/kids.js
@@ -1,143 +1,35 @@
 import { connect } from 'react-redux';
-import { store } from '../store';
-import { getWorkshops } from '../actions/workshops';
-import { Page, PageHeading, PageBody } from '../components/page';
+import { Page, PageHeading, Container } from '../components/page';
 import { Block, BlockHeading, Columns, Column, BackgroundImage, ColumnHeading, P } from '../components/textblock';
 import { Dl, Dt, Dd } from '../components/definition-list';
-import header from '../assets/kids/kids_header.jpg';
-import cannybots from '../assets/kids/cannybots.jpg';
+import header from '../assets/kids/kids_header2.jpg';
+import mbot from '../assets/kids/mbot.jpg';
 import codestudio from '../assets/kids/codestudio.jpg';
 import minecraft from '../assets/kids/minecraft.jpg';
-import processing from '../assets/kids/processing.jpg';
-
-const workshopData = {
-    kidsakodervaar2016cannybots: {
-        description: [
-            <span><a href='http://cannybots.com'>Cannybots</a> er leketøyroboter som du kan printe ut ved hjelp av en vanlig hjemme-3D-printer. Du kan styre den fra en telefon, nettbrett, PC eller til Raspberry Pi og programmere den ved hjelp av enkle programmeringsgrensesnitt. Barna skal begynne med litt grunnleggende programmering i Scratch. Kjør roboten frem, sving, kjør i ring osv.</span>,
-            'Når de har gjort dette kan de få tilgang til eksisterende (og litt avanserte) programmer for autonom eller fjernstyrt kjøring. Vi går først i gjennom og forklarer hva programmet gjør. Deretter prøver vi litt og så ser vi på koden igjen for å se om barna ser sammenhengen mellom koden og hva roboten gjør.'
-        ],
-        age: '8 år +',
-        image: cannybots,
-        left: true
-    },
-    kidsakodervaar2016codestudio: {
-        description: [
-            'Programmering med CodeStudio sine oppgaver for de aller yngste. Her vil vi ta i bruk Code.org sine opplegg for Kodetimen. Dette vil bli en lett introduksjon som verken krever forkunnskaper i lesing eller matematikk.',
-            <span>Oppgavene finnes på <a href='http://studio.code.org'>studio.code.org</a>.</span>
-        ],
-        age: '4-10 år',
-        image: codestudio
-    },
-    kidsakodervaar2016minecraft: {
-        description: [
-            <span>ComputerCraft er en mod til Minecraft, som lar deg bygge datamaskiner og roboter inne i spillet. Disse datamaskinene og robotene kan programmeres til å kontrollere dører, grave huler, bygge hus og så videre. ComputerCraft bruker programmeringsspråket Lua, som er et enkelt og fleksibelt tekstbasert programmeringsspråk. For å delta på dette kurset må deltagerne ha en Minecraft-konto. <a href='https://github.com/kodeklubben/oppgaver/tree/master/src/computercraft'>Oppskrift for hvordan du installerer ComputerCraft</a>.</span>
-        ],
-        age: '10 år +',
-        image: minecraft,
-        left: true
-    },
-    kidsakodervaar2016processing: {
-        description: [
-            'Her ønskes ungdommene velkommen til et minikurs hvor de kan lære seg å programmere. Dette kurset er rettet mot nybegynnere og krever ingen forhåndskunnskaper. Målet for kurset er å gå gjennom noen viktige begreper innenfor programmering og at alle skal lære seg nok til å lage et visuelt program.',
-            'Processing er et språk og utviklingsmiljø som gjør det enkelt å komme i gang med programmering. Det kan lastes ned via processing.org/download. Processing er basert på Java, så hvis du lærer deg Processing, så vil du indirekte lære deg litt Java.'
-        ],
-        age: '14 år +',
-        image: processing
-    }
-};
-
-function workshopUrl(id) {
-    return `https://javazone.no/moosehead/#/register/${id}`;
-}
-
-function workshopClass(status) {
-    switch (status) {
-    case 'FREE_SPOTS': return 'button--green';
-    case 'FEW_SPOTS': return 'button--yellow';
-    case 'FULL': return 'button--red';
-    case 'VERY_FULL': return 'button--red';
-    case 'CLOSED': return 'button--disabled';
-    default: return 'button--disabled';
-    }
-}
-
-function workshopStatus(status) {
-    switch (status) {
-    case 'FREE_SPOTS': return 'Meld deg på nå';
-    case 'FEW_SPOTS': return 'Få plasser igjen, meld deg på nå';
-    case 'FULL': return 'Påmelding m/ venteliste';
-    case 'VERY_FULL': return 'Ingen ledige plasser';
-    case 'CLOSED': return 'Påmelding stengt';
-    default: return 'Påmelding ikke åpnet';
-    }
-}
-
-const LeftEvent = ({workshop}) => (
-    <Block>
-        <Column justify={true} dark={true}>
-            <ColumnHeading className='kids__event-title'>{workshop.title}</ColumnHeading>
-            <span className='kids__age'>{workshopData[workshop.id].age}</span>
-            {workshopData[workshop.id].description.map((desc, dk) => (
-                <P key={dk}>
-                    {desc}
-                </P>
-            ))}
-            <a href={workshopUrl(workshop.id)} className={`button ${workshopClass(workshop.status)} kids__registrate`}>
-                {workshopStatus(workshop.status)}
-            </a>
-        </Column>
-        <Column>
-            <BackgroundImage src={workshopData[workshop.id].image} />
-        </Column>
-    </Block>
-);
-
-const RightEvent = ({workshop}) => (
-    <Block>
-        <Column>
-            <BackgroundImage src={workshopData[workshop.id].image} />
-        </Column>
-        <Column justify={true} dark={true}>
-            <ColumnHeading className='kids__event-title'>{workshop.title}</ColumnHeading>
-            <span className='kids__age'>{workshopData[workshop.id].age}</span>
-            {workshopData[workshop.id].description.map((desc, dk) => (
-                <P key={dk}>
-                    {desc}
-                </P>
-            ))}
-            <a href={workshopUrl(workshop.id)} className={`button ${workshopClass(workshop.status)} kids__registrate`}>
-                {workshopStatus(workshop.status)}
-            </a>
-        </Column>
-    </Block>
-);
+import raspberrypi from '../assets/kids/raspberrypi.jpg';
 
 function mapStateToProps(state) {
-    return {
-        workshops: state.workshops.workshops
-    };
+    return {};
 }
 
 const Kids = React.createClass({
     componentWillMount() {
-        this.props.getWorkshops();
     },
 
     render() {
-        const workshops = this.props.workshops;
 
         return (
             <Page name='kids'>
                 <PageHeading background={header}>JavaZone Kids</PageHeading>
-                <PageBody>
+                <Container>
                     <Block block={true}>
-                        <BlockHeading center={true}>A taste of JavaZone for young, aspiring developers</BlockHeading>
+                        <BlockHeading center={true}>En smakebit på JavaZone for fremtidens utviklere</BlockHeading>
                         <Columns>
                             <Column justify={true}>
                                 <P>
                                     JavaZone inviterer alle deltakere på JavaZone
                                     til å ta med seg sine barn på Teknologihuset
-                                    søndag 13. mars for å gi dem en smakebit på
+                                    søndag 4. september for å gi dem en smakebit på
                                     voksenlivet. Vi kjører 4 parallelle sesjoner,
                                     slik at vi har noe for både de aller minste
                                     barna og de største ungdommene.
@@ -147,8 +39,11 @@ const Kids = React.createClass({
                                 <P>
                                     Opplegget arrangeres i samarbeid med <a href='http://www.kidsakoder.no/'>Lær Kidsa Koding</a> som
                                     har lang erfaring med å arrangere kodeklubber for de
-                                    minste, og <a href='https://bitraf.no/'>Bitraf</a> som
-                                    er Oslos fremste eksperter på bygging av kule ting.
+                                    minste, <a href='https://bitraf.no/'>Bitraf</a> som
+                                    er Oslos fremste eksperter på bygging av kule ting,
+                                    og <a href="https://kodegenet.no">Kodegenet</a> som til
+                                    daglig arrangerer spennende og
+                                    engasjerende kursopplegg for barn og unge.
                                 </P>
                             </Column>
                         </Columns>
@@ -162,7 +57,7 @@ const Kids = React.createClass({
                                     <Dt>Tid og sted</Dt>
                                     <Dd>
                                         Arrangementet finner sted på Teknologihuset
-                                        søndag 13. mars fra klokken 12.00 til
+                                        søndag 4. september fra klokken 12.00 til
                                         15.00. Vi serverer enkel mat underveis. Det
                                         er viktig at du som forelder også setter av
                                         tid til å være til stede hele perioden. Vi
@@ -178,10 +73,9 @@ const Kids = React.createClass({
                                 <Dl>
                                     <Dt>Påmelding</Dt>
                                     <Dd>
-                                        Vi har et begrenset antall plasser (totalt
-                                        plass til 82 barn), derfor er det viktig at
+                                        Vi har et begrenset antall plasser derfor er det viktig at
                                         du melder dere på arrangementet.
-                                        Påmeldingen åpner fredag 12.februar kl. 12.00.
+                                        Påmeldingen åpner fredag 5.august kl. 12.00.
                                     </Dd>
                                     <Dt>Forberedelser</Dt>
                                     <Dd>
@@ -206,26 +100,107 @@ const Kids = React.createClass({
                                     kan du melde deg på flere sesjoner – sørg i så fall
                                     for å melde på riktig antall barn til hver sesjon.
                                 </P>
+                                <P>
+                                    Påmeldingen åpner fredag 5.august kl. 12.00.
+                                </P>
                             </Column>
                         </Columns>
                     </Block>
 
-                    {workshops.map((workshop, key) => (
-                        workshopData[workshop.id].left ? <LeftEvent workshop={workshop} key={key} /> : <RightEvent key={key} workshop={workshop} />
-                    ))}
+                    <Block>
+                        <Column justify={true} dark={true}>
+                            <ColumnHeading className='kids__event-title'>CodeStudio</ColumnHeading>
+                            <span className='kids__age'>4-10 år</span>
+                            <P>
+                                Programmering med CodeStudio sine oppgaver for de aller yngste. Her vil vi ta i bruk <a href="https://code.org">Code.org</a> sine opplegg for Kodetimen. Dette vil bli en lett introduksjon som verken krever forkunnskaper i lesing eller matematikk.
+                            </P>
+                            <P>
+                                Oppgavene finnes på <a href="https://studio.code.org">studio.code.org</a>.
+                            </P>
+                            <a className={`button button--disabled kids__registrate`}>
+                                Påmeldingen åpner fredag 5.august kl. 12.00
+                            </a>
+                        </Column>
+                        <Column>
+                            <BackgroundImage src={codestudio} />
+                        </Column>
+                    </Block>
+
+                    <Block>
+                        <Column>
+                            <BackgroundImage src={minecraft} />
+                        </Column>
+                        <Column justify={true} dark={true}>
+                            <ColumnHeading className='kids__event-title'>ComputerCraft i Minecraft</ColumnHeading>
+                            <span className='kids__age'>11+ år</span>
+                            <P>
+                                ComputerCraft er en mod til Minecraft, som lar deg bygge datamaskiner og roboter inne i spillet. Disse datamaskinene og robotene kan programmeres til å kontrollere dører, grave huler, bygge hus og så videre. ComputerCraft bruker programmeringsspråket Lua, som er et enkelt og fleksibelt tekstbasert programmeringsspråk. For å delta på dette kurset må deltagerne ha en Minecraft-konto.
+                            </P>
+                            <P>
+                                <a href="https://github.com/kodeklubben/oppgaver/tree/master/src/computercraft">Oppskrift for hvordan du installerer ComputerCraft</a>.
+                            </P>
+                            <a className={`button button--disabled kids__registrate`}>
+                                Påmeldingen åpner fredag 5.august kl. 12.00
+                            </a>
+                        </Column>
+                    </Block>
+
+                    <Block>
+                        <Column justify={true} dark={true}>
+                            <ColumnHeading className='kids__event-title'>Lær å programmere roboter</ColumnHeading>
+                            <span className='kids__age'>10-14 år</span>
+                            <P>
+                                Bygg og styr en <a href="http://makeblock.com/mbot-stem-educational-robot-kit-for-kids/">mBot</a> både med en app og helt av seg selv. Vi får roboten til å bruke motor, sensorer, lys og lyd mens vi lærer hvordan man kan styre den.
+                            </P>
+                            <P>
+                                Se også <a href="https://www.youtube.com/watch?v=pmsSipper3Y">videoen som forklarer mBot</a>.
+                            </P>
+                            <P>
+                                Vi bruker programmet <a href="http://www.mblock.cc">mBlock</a> som er basert på Scratch. Scratch ble laget på MIT for å lære barn å programmere.
+                            </P>
+                            <a className={`button button--disabled kids__registrate`}>
+                                Påmeldingen åpner fredag 5.august kl. 12.00
+                            </a>
+                        </Column>
+                        <Column>
+                            <BackgroundImage src={mbot} />
+                        </Column>
+                    </Block>
+
+                    <Block>
+                        <Column>
+                            <BackgroundImage src={raspberrypi} />
+                        </Column>
+                        <Column justify={true} dark={true}>
+                            <ColumnHeading className='kids__event-title'>Raspberry Pi og Java</ColumnHeading>
+                            <span className='kids__age'>12+ år</span>
+                            <P>
+                                Raspberry Pi er kanskje verdens aller kuleste mini-datamaskin. Datamaskinen kjører Linux, og kan dermed programmeres med alle verdens programmeringsspråk. I løpet av dette kurset skal vi programmere Raspberry Pi maskinen ved hjelp av Java og utviklerverktøyet Eclipse.
+                            </P>
+                            <P>
+                                Vi starter med å lære det aller mest grunnleggende, hvordan vi bruker operativsystemet Raspian, og hvordan vi navigerer i filsystemet. Deretter skal vi ta frem litt elektroniske komponenter som koblingsbrett, LED lamper, knapper, motstandere og kanskje en motor og styre disse ved hjelp av Raspberry Pi maskinen.
+                            </P>
+                            <P>
+                                Opplegget vil være basert på Raspberry Pi sidene til <a href="https://kodegenet.no">Kodegenet.no</a>.
+                            </P>
+                            <a className={`button button--disabled kids__registrate`}>
+                                Påmeldingen åpner fredag 5.august kl. 12.00
+                            </a>
+                        </Column>
+                    </Block>
 
                     <Block className='kids__greeting'>
                         <Column center={true}>
-                            <ColumnHeading>Vi sees på Teknologihuset søndag 13. mars!</ColumnHeading>
+                            <ColumnHeading>Vi sees på Teknologihuset søndag 4. september!</ColumnHeading>
                             <P>
-                                Vennlig hilsen,<br />javaBin, Lær Kidsa Koding og Bitraf.
+                                Vennlig hilsen,<br />javaBin, Lær Kidsa Koding, Bitraf & Kodegenet.
                             </P>
                         </Column>
                     </Block>
-                </PageBody>
+                </Container>
             </Page>
         );
     }
 });
 
-export default connect(mapStateToProps, { getWorkshops })(Kids);
+export default connect(mapStateToProps, { })(Kids);
