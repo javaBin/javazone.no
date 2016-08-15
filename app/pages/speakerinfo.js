afac8e319a4ba02d8098c72f4ea3cc7bcf33c720
Bendik Solheim <bendik@solheim.im>
2016-08-15 23:33:10 +0200

    Remove trailing spaces

--- app/pages/speakerinfo.js
+++ app/pages/speakerinfo.js
@@ -44,21 +44,21 @@ export default () => (
                     <SubHeader>Guided Tour</SubHeader>
 
                     <p>
-                        Never been to JavaZone, want to check out the room you'll be speaking in, or just curious to see the venue? We will have a guided tour for speakers on Tuesday September 6th, 18:00. Meet us at the <a href={mainEntrance}>main entrance</a>.
+                        Never been to JavaZone, want to check out the room you’ll be speaking in, or just curious to see the venue? We will have a guided tour for speakers on Tuesday September 6th, 18:00. Meet us at the <a href={mainEntrance}>main entrance</a>.
                     </p>
 
                     <SubHeader>Speakers Dinner</SubHeader>
 
                     <p>
-                        This years speakers dinner will be held at <a href='http://www.grilleriet.no/english/'>Grilleriet</a> (located at <a href={grilleriet}>Folketeaterpassasjen, Storgata 21</a>), on Tuesday September 6th, starting at 19:00. After the tour we’ll head straight for the Speakers Dinner, which is located less than 10 minutes walk from the JavaZone venue. If you don’t want to attend the guided tour, it is perfectly ok to show up at Grilleriet from 19:00 onwards. And it's no problem if you are a little late.
+                        This years speakers dinner will be held at <a href='http://www.grilleriet.no/english/'>Grilleriet</a> (located at <a href={grilleriet}>Folketeaterpassasjen, Storgata 21</a>), on Tuesday September 6th, starting at 19:00. After the tour we’ll head straight for the Speakers Dinner, which is located less than 10 minutes walk from the JavaZone venue. If you don’t want to attend the guided tour, it is perfectly ok to show up at Grilleriet from 19:00 onwards. And it’s no problem if you are a little late.
                     </p>
-                    
+
                     <SubHeader>Cost coverage</SubHeader>
 
                     <p>
                         If you sucessfully applied for cost coverage, please send us a copy of your receipts <b>after</b> the conference to <a href="mailto:program@java.no">program@java.no</a>. If we agreed upon covering your expenses, we have reserved a room at <a href="http://www.thonhotels.com/de/hotels/lander/norwegen/oslo/thon-hotel-terminus/">Thon Hotel Terminus</a> for you. The room is prepayed and reserved on your name.
                     </p>
-                    
+
                     <SubHeader>Visa invitation</SubHeader>
 
                     <p>
