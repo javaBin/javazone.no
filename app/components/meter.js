eab411692c0f72cf471689a6f5a40bb781d0706c
Ingar Abrahamsen <ingar@abrahams1.com>
2016-10-11 21:18:32 +0200

    add meter component

---
+++ app/components/meter.js
@@ -0,0 +1,25 @@
+import React from "react";
+import className from "classname";
+
+export const Meter = (props) => {
+    const pillarHeight = {height: (props.value / props.max * 100)};
+    const between = (v, min, max) => (v.height >= min && v.height < max);
+    const c = className(
+        "meter__pillar",
+        {
+            "meter__pillar--green": between(pillarHeight, 60, 101),
+            "meter__pillar--yellow": between(pillarHeight, 25, 50),
+            "meter__pillar--red": between(pillarHeight, 0, 25)
+        });
+    return (
+        <div className="meter__container">
+            <div className={c} style={pillarHeight}></div>
+        </div>
+    );
+};
+Meter.desplayName = "Meter";
+Meter.propTypes = {
+    max: React.PropTypes.number,
+    min: React.PropTypes.number,
+    value: React.PropTypes.number
+};
