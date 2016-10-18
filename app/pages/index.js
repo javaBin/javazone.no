bb66063ec850faa0812099aa0f853d3aa991a29b
Bendik Solheim <bendik@solheim.im>
2016-10-18 20:47:47 +0200

    Few small updates to index page

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from '../assets/logo-horisontal-hvit.svg';
+import background from '../assets/background.jpg';
 import { Page, PageHeading, Container } from '../components/page';
 import { CenteredBlock, CenteredHeader, CenteredContent } from '../components/centeredblock';
 
 const Index = () => (
     <Page name='index'>
-        <div className='index__content'>
+        <div className='index__content' style={{backgroundImage: `url(${background})`}}>
             <h1 className='index__header'>2</h1>
             <div className='index__text'>
-                What?
+                September. 13th & 14th. 2017
             </div>
         </div>
     </Page>
