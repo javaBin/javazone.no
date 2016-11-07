3072c25c01d2629f43ba1575cf118c4744c3a7ef
Bendik Solheim <bendik@solheim.im>
2016-11-07 19:55:55 +0100

    Remove two now unused files

--- app/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { workshops } from './workshops';
-import { sessions } from './sessions';
-import { session } from './session';
-import { routes } from './routes';
-
-export { workshops, sessions, session, routes };
