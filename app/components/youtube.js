2612b20426398b120a25a77d6bcd5aa82fa1147a
Bendik Solheim <bendik@solheim.im>
2016-11-08 21:14:48 +0100

    Responsive youtube videos

--- app/components/youtube.js
+++ app/components/youtube.js
@@ -1,8 +1,3 @@
-const styles = {
-    maxWidth: '100%',
-    width: '100%'
-};
-
 export default ({id}) => (
-    <iframe style={styles} width="100%" height="675" src={`https://www.youtube.com/embed/${id}?rel=0&showinfo=0`} frameBorder="0" allowFullScreen></iframe>
+    <iframe className='youtube' src={`https://www.youtube.com/embed/${id}?rel=0&showinfo=0`} frameBorder="0" allowFullScreen></iframe>
 );
