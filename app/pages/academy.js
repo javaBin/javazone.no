3addbf9b369c2e99a4de11da583ed1cfa7f97b48
Bendik Solheim <bendik@solheim.im>
2017-01-23 21:52:59 +0100

    Add information to academy page

--- app/pages/academy.js
+++ app/pages/academy.js
@@ -9,48 +9,117 @@ import samfundet from '../assets/academy/samfundet.jpg';
 const oslo = {
     image: teknologihuset,
     city: 'Oslo',
-    date: 'February 16th',
+    date: 'February 14th',
     location: 'Teknologihuset',
     location_link: 'http://www.teknologihuset.no',
     pax: 120,
     program: 'academy-oslo',
     registration: [
-        {text: 'Register for Academy Oslo', link: 'https://docs.google.com/forms/d/1JfeJsALw_6Dur0br1uVxQ7KAe0neKlTZAIVoxO7zFBE/viewform?usp=send_form'}
+        {text: 'Register for Academy Oslo', link: 'https://docs.google.com/forms/d/e/1FAIpQLSdCD9dm8F7771fEHHXuTRLejLLTrhbTzYCnkQYWu3G-hFqh7A/viewform?c=0&w=1'}
     ]
 };
 
 const bergen = {
     image: kvarteret,
     city: 'Bergen',
-    date: 'February 17th',
+    date: 'February 16th',
     location: 'Det Akademiske Kvarter',
     location_link: 'http://kvarteret.no/',
     pax: 120,
     program: 'academy-bergen',
     registration: [
-        {text: 'Register for Academy Bergen', link: 'https://docs.google.com/forms/d/1XsRJ-77y0YjrhWh6QkzN8NS2qwOD7EEOY4zzaoZuP0s/viewform?usp=send_form'},
+        {text: 'Register for Academy Bergen', link: 'https://docs.google.com/forms/d/e/1FAIpQLSfK6Lwo_5fRLTbzuzZKNuUUS2bo2O_TLnQswpwQXkVndv9GYg/viewform?c=0&w=1'},
     ]
 };
 
 const trondheim = {
     image: samfundet,
     city: 'Trondheim',
-    date: 'February 18th',
+    date: 'February 15th',
     location: 'Studentersamfundet',
     location_link: 'https://www.samfundet.no/',
     pax: 200,
     program: 'academy-trondheim',
     registration: [
-        {text: 'Register @ Abakus', link: 'https://abakus.no/event/1693-javazone-academy-2016/'},
-        {text: 'Register @ Online', link: 'https://online.ntnu.no/events/254/javazone-academy-2016/'}
+        {text: 'Register for Academy Trondheim', link: 'https://abakus.no/event/1946-javazone-academy-2017/'},
     ]
 };
+
+const Location = ({location}) => (
+    <div className='location'>
+        <div className='location__image' style={{backgroundImage: `url('${location.image}')`}}>
+            <h3 className='location__title'>{location.city}</h3>
+        </div>
+        <p className='location__date'>
+            <strong>{location.date}</strong>
+        </p>
+        <p className='location__location'>
+            <a href={location.location_link}>
+                {location.location}
+            </a>
+        </p>
+        <p className='location__pax'>
+            {location.pax} students
+        </p>
+        {location.registration.map((reg, key) => (
+             <p key={key}>
+                 <a className='button button--transparent' href={reg.link}>
+                    {reg.text}
+                 </a>
+             </p>
+        ))}
+    </div>
+);
 
 const Academy = () => (
     <Page name='academy'>
         <Heading>
             <LargeHeading>JavaZone Academy</LargeHeading>
         </Heading>
+
+        <Container>
+            <CBlock>
+                <CContent>
+                    <P>
+                        Are you a student? Interested in IT? Come join us for JavaZone Academy.
+                        JavaZone Academy is a free event for students in Norway, where we invite you
+                        to take part in the Javazone experience.
+                    </P>
+                    <P>
+                        We will bring great talks, food, mingling and great partners to
+                        <strong>Oslo</strong>, <strong> Bergen</strong> and
+                        <strong>Trondheim</strong>. Join us for a great evening!
+                    </P>
+                </CContent>
+            </CBlock>
+        </Container>
+
+        <ul className='academy__locations'>
+            <Location location={oslo} />
+            <Location location={bergen} />
+            <Location location={trondheim} />
+        </ul>
+
+        <Container>
+            <Block>
+                <Header>Program and Speakers</Header>
+                <Content>
+                    <P>
+                        JavaZone Academy starts at 1230 (doors open at 1200), and lasts until 1730.
+                        Afterwards, there will be food and drinks.
+                        There will be 4 or 5 presentations at each location.
+                        The exact program will be available once the event is approacing.
+                    </P>
+                    <SubHeader>How do I register?</SubHeader>
+                    <P>
+                        Use the signup links above to sign up for your prefered town.
+                        We’ll contact you as soon as possible with information about
+                        whether you got a spot.
+                        The events are usually quite popular, so make sure to registrer as soon as possible.
+                    </P>
+                </Content>
+            </Block>
+        </Container>
     </Page>
 );
 
