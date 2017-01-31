15240498ae79d9e12358d798a307b9aff6ea0f81
Helge Jenssen <helge.jenssen@gmail.com>
2017-01-31 16:45:29 +0100

    Update academy.js
    Added program

--- app/data/academy.js
+++ app/data/academy.js
@@ -8,13 +8,44 @@ export default {
                 title: 'Doors open'
             },
             {
-                time: '1200-1230',
+                time: '1200-1245',
                 title: 'Intro'
             },
             {
-                time: '1230-1330',
-                title: 'Test talk',
-                speaker: 'Test speaker'
+                time: '1245-1345',
+                title: 'Personal security',
+                speaker: 'Michael Johansen'
+            },
+            {
+                time: '1345-1415',
+                title: 'Break'
+            },
+            {
+                time: '1415-1515',
+                title: 'How We Built an Indoor Map For the JavaZone App. You Can Do That Too!',
+                speaker: 'Khiem-Kim Ho Xuan, Rustam Mehmandarov'
+            },
+            {
+                time: '1515-1545',
+                title: 'Break'
+            },
+            {
+                time: '1545-1630',
+                title: 'Machine learning at NRK: From prototype to production',
+                speaker: 'Øyvind Holmstad, Thomas Oldervoll'
+            },
+            {
+                time: '1630-1700',
+                title: 'Break'
+            },
+            {
+                time: '1700-1800',
+                title: 'The Secret Sauce of Successful Teams',
+                speaker: 'Sven Peters'
+            },
+            {
+                time: '1800-late',
+                title: 'AweZone Party!'
             }
         ]
     },
@@ -27,13 +58,44 @@ export default {
                 title: 'Doors open'
             },
             {
-                time: '1200-1230',
+                time: '1200-1245',
                 title: 'Intro'
             },
             {
-                time: '1230-1330',
-                title: 'Test talk',
-                speaker: 'Test speaker'
+                time: '1245-1330',
+                title: 'Machine learning at NRK: From prototype to production',
+                speaker: 'Thomas Oldervoll'
+            },
+            {
+                time: '1330-1400',
+                title: 'Break'
+            },
+            {
+                time: '1400-1500',
+                title: 'The Secret Sauce of Successful Teams',
+                speaker: 'Sven Peters'
+            },
+            {
+                time: '1500-1530',
+                title: 'Break'
+            },
+            {
+                time: '1530-1630',
+                title: 'Personal security',
+                speaker: 'Michael Johansen'
+            },
+            {
+                time: '1630-1700',
+                title: 'Break'
+            },
+            {
+                time: '1700-1800',
+                title: 'How We Built an Indoor Map For the JavaZone App. You Can Do That Too!',
+                speaker: 'Khiem-Kim Ho Xuan, Rustam Mehmandarov'
+            },
+            {
+                time: '1800-late',
+                title: 'AweZone Party!'
             }
         ]
     },
@@ -42,17 +104,48 @@ export default {
         date: 'February 16th',
         program: [
             {
-                time: '1130',
+                time: '1230',
                 title: 'Doors open'
             },
             {
-                time: '1200-1230',
+                time: '1230-1315',
                 title: 'Intro'
             },
             {
-                time: '1230-1330',
-                title: 'Test talk',
-                speaker: 'Test speaker'
+                time: '1315-1415',
+                title: 'The Secret Sauce of Successful Teams',
+                speaker: 'Sven Peters'
+            },
+            {
+                time: '1415-1445',
+                title: 'Break'
+            },
+            {
+                time: '1445-1545',
+                title: 'Personal security',
+                speaker: 'Michael Johansen'
+            },
+            {
+                time: '1545-1600',
+                title: 'Break'
+            },
+            {
+                time: '1600-1700',
+                title: 'How We Built an Indoor Map For the JavaZone App. You Can Do That Too!',
+                speaker: 'Khiem-Kim Ho Xuan, Rustam Mehmandarov'
+            },
+            {
+                time: '1700-1715',
+                title: 'Break'
+            },
+            {
+                time: '1715-1800',
+                title: 'Machine learning at NRK: From prototype to production',
+                speaker: 'Øyvind Holmstad'
+            },
+            {
+                time: '1800-late',
+                title: 'AweZone Party!'
             }
         ]
     }
