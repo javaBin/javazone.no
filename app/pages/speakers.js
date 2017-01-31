2463e45ec4e585f8671008f87912f19f33facbb1
Kjetil Valle <kjetil.valle@gmail.com>
2017-01-31 20:07:51 +0100

    Text for Call for Speakers page from wiki

--- app/pages/speakers.js
+++ app/pages/speakers.js
@@ -18,21 +18,21 @@ const lightnings = {
     icon: 'icon-energy',
     title: 'Lightning talks',
     length: '10 or 20 minutes',
-    description: 'Are you presenting a great new idea, or want to give the audience a teaser for a cool topic? Then you should strongly consider the lightning format.  Note that the 10/20 minute time limit is strictly enforced!'
+    description: 'Are you presenting a great new idea, or want to give the audience a teaser for a cool topic? Then you should strongly consider the lightning talk format.  Note that the 10-20 minute time limit is strictly enforced!'
 };
 
 const presentations = {
     icon: 'icon-graduation',
     title: 'Presentations',
     length: '45 or 60 minutes',
-    description: 'Presentations at JavaZone have a length of 45 to 60 minutes, including time for questions. This gives you room to both elaborate on an idea and interact with your audience.'
+    description: 'Presentations at JavaZone can be either 45 or 60 minutes long. This gives you room to elaborate on an idea. When submitting your talk, please indicate clearly in the outline how much time is reserved for questions.'
 };
 
 const workshops = {
     icon: 'icon-screen-desktop',
     title: 'Workshops',
-    length: '2 hours +',
-    description: 'We will continue the popular workshop concept, both half-day and whole-day workshops on Tuesday September 6th and shorter 2-hour workshops during the conference. The format for the workshops is in-depth, hands-on and interactive.'
+    length: '2 hours, half a day, whole day',
+    description: 'We will continue the popular workshop concept with a range of sessions on Tuesday September 12th (and also possibly during the conference). The format for the workshops is in-depth, hands-on and interactive.'
 };
 
 const Speakers = () => (
@@ -44,37 +44,22 @@ const Speakers = () => (
 
         <Container>
             <CBlock>
-                <CHeader>What is JavaZone?</CHeader>
+                <CHeader>About JavaZone</CHeader>
                 <CContent>
                     <P>
-                        JavaZone takes place in Oslo, Norway, on September 7th-8th
-                        2015. The conference offers a combination of technical talks
-                        and workshops in an informal atmosphere with an expected
-                        attendance of more than 2500 highly skilled developers.
+                        JavaZone takes place in Oslo, Norway, on September 13th-14th 2017. The conference offers a combination of technical talks and workshops in an informal atmosphere with an expected attendance of up to 3000 developers.
                     </P>
                     <P>
-                        JavaZone 2016 will be the 15th consecutive JavaZone conference,
-                        and will consist of a day of workshops followed by two days
-                        of presentations and (more) workshops. Last year’s event was
-                        a great success with more than 2500 attendees enjoying 150
-                        talks covering a wide range of topics.  Speakers include both
-                        internationally known names and home grown Norwegian talent.
+                        JavaZone 2017 will be the 2^4th consecutive JavaZone conference, and will consist of a day of workshops followed by two days of presentations and (more) workshops. Last year's event was a great success with more than 3000 attendees enjoying 173 talks covering a wide range of topics. JavaZone speakers include both internationally renowned speakers and home-grown Norwegian talent. 
                     </P>
                     <P>
-                        Over the last decade JavaZone has grown to become one of
-                        the most important Java events in Europe. You can see for
-                        yourself – videos from previous years are available for
-                        free at <a href='https://vimeo.com/javazone'>Vimeo</a>, and
-                        the full program from JavaZone 2015 can be found
-                        at <a href='http://2015.javazone.no/program.html'>last year’s web page</a>.
+                    	Over the last decade JavaZone has grown to become one of the most important Java events in Europe! You can see for yourself – videos from previous years are available for free at <a href='https://vimeo.com/javazone'>Vimeo</a>, and the full program from JavaZone 2016 can be found on <a href='https://2016.javazone.no/program'>last year’s web page</a>.
                     </P>
                     <P>
-                        We can brag as much as we want, but JavaZone would be
-                        nothing without all the great speakers. And that is why
-                        we need your help to make sure that JavaZone 2016 will be
-                        at least as awesome as the earlier events. If you think you
-                        have something interesting to share, please submit your
-                        talk or workshop by April 18th 2016.
+                        We can brag as much as we want, but JavaZone would be nothing without all the great speakers! And that is why we need your help to make sure that JavaZone 2017 will be at least as awesome as the earlier events. If you think you have something interesting to share, please submit your talk or workshop by the 24th of April, 2017.
+                    </P>
+                    <P>
+                    	JavaZone is committed to diversity, and we are especially interested in quality submissions from groups traditionally under-represented in tech.
                     </P>
                 </CContent>
             </CBlock>
@@ -92,7 +77,7 @@ const Speakers = () => (
                 <div className='speakers__formats'>
                     <span className='icon-clock'></span>
                 </div>
-                <CHeader>Formats and Duration</CHeader>
+                <CHeader>Formats and Durations</CHeader>
                 <CContent>
                     <P>
                         You should think about which format your talk will work
@@ -113,26 +98,35 @@ const Speakers = () => (
             </Block>
 
             <Block>
-                <Header>Technical Level</Header>
+                <Header>Audience and Topics</Header>
                 <Content>
                     <P>
-                        The audience at JavaZone mostly consists of software
-                        developers and in general talks and workshops should
-                        be technical enough that they include code (although
-                        that is not a requirement). Talks purely related to
-                        practices (agile etc.) are less likely to be accepted.
+                        JavaZone is a conference for developers and technical architects, with an emphasis on technical talks. However, we are open to talks about other areas, including methodology, project management, and other topics related to programming.
                     </P>
                     <P>
-                        When you submit your talk, you should indicate whether
-                        there will be code in your slides or not. If you are going
-                        to perform live coding, please state that as well!
+                        Talks do not have to be about JVM technology. We will consider all talks based on their relevance to developers and technical architects.
                     </P>
+                    <P>
+                    	We are always interested in Java technology and its applications, current trends in programming, and experience reports. Topics we would like to hear more about this year include:
+                    </P>
+                    <ul>
+                	    <li>Front-end patterns and architectures.</li>
+					    <li>Practical functional programming.</li>
+					    <li>Machine learning, AI, deep learning.</li>
+					    <li>Hacking, security, cryptography, blockchains.</li>
+                	</ul>
+                	<P>
+                		... and, of course, your killer topic.
+                	</P>
+                	<P>
+                		When you submit your talk, you should indicate whether there will be code in your slides or not. If you are going to perform live coding, please state that as well!
+                	</P>
                     <SubHeader>Get Some Inspiration</SubHeader>
                     <dl className='speakers__inspiration'>
                         <dt className='speakers__inspiration-title'>Speakers workshop</dt>
-                        <dd className='speakers__inspiration-description'>We will arrange a workshop for all speakers who consider submitting a talk to JavaZone. This workshop will be held at Teknologihuset March 1st at 17.00. More info at <a href='http://www.meetup.com/javaBin/events/228728923/'>meetup.com/javaBin</a>.</dd>
+                        <dd className='speakers__inspiration-description'>On the 7th of March we are hosting a <a href="https://www.meetup.com/javaBin/events/237342945/">workshop for all those who are considering submitting a talk to JavaZone</a>.</dd>
                         <dt className='speakers__inspiration-title'>Presentation skills workshop</dt>
-                        <dd className='speakers__inspiration-description'>We plan to have a professional coach which will give you a lot of tips and tricks on how to hold an amazing presentation. This session will be held in August, and all accepted speakers will be invited. More information coming soon.</dd>
+                        <dd className='speakers__inspiration-description'>JavaZone has a strong tradition of nurturing local speaker talent. To help with this we arrange a presentation skills workshop 2-3 weeks before JavaZone. This workshop is held in Oslo and is free for all accepted speakers. This workshop will be held in Norwegian.</dd>
                     </dl>
                 </Content>
             </Block>
@@ -149,10 +143,10 @@ const Speakers = () => (
                 <Content>
                     <dl className='speakers__accepted'>
                         <dt className='speakers__accepted-title'>Accepted presentations and workshops</dt>
-                        <dd className='speakers__accepted-description'>You get free admission to the conference. Additionally you are also invited to the speakers' dinner, held on September 6th. Finally, you have first priority on the limited number of slots for JourneyZone, our annual speaker adventure.</dd>
+                        <dd className='speakers__accepted-description'>You get free admission to the conference. Additionally you are also invited to the speakers' dinner, held on September 12th. Finally, you have first priority on the limited number of slots for JourneyZone, our annual speaker adventure.</dd>
                         <dt className='speakers__accepted-title'>Accepted lightning talks</dt>
-                        <dd className='speakers__accepted-description'>You are invited to the speakers' dinner, held on September 6th, and will also be invited to apply for a place on our JourneyZone. You have to provide your own ticket to JavaZone.</dd>
-                        <dt className='speakers__accepted-title'>Monetary policy</dt>
+                        <dd className='speakers__accepted-description'>You are invited to the speakers' dinner, held on September 12th, and will also be invited to JourneyZone. You have to provide your own ticket to JavaZone.</dd>
+                        <dt className='speakers__accepted-title'>Coverage of Expenses</dt>
                         <dd className='speakers__accepted-description'>Please see our <Link to='monetary-policy' className='page__link'>monetary policy</Link> if you have any questions regarding coverage of other expenses.</dd>
                     </dl>
                 </Content>
@@ -162,19 +156,15 @@ const Speakers = () => (
                 <Header>Important principles</Header>
                 <Content>
                     <P>
-                        JavaZone is proud to be an independent and community-driven
-                        conference. It is organized by javaBin, the idealistic and
-                        non-profit Norwegian Java User Group. This means that we
-                        have three important principles for selecting talks.
+                        JavaZone is proud to be an independent and community-driven conference. It is organized by volunteers from javaBin (the Norwegian Java User Group) and is run as a non-profit organisation. This means that we have three important principles for selecting talks:
                     </P>
                     <ul className='speakers__principles'>
-                        <li className='speakers__principle'>We do not sell speaker slots</li>
+                        <li className='speakers__principle'>We do not sell speaker slots.</li>
                         <li className='speakers__principle'>We do not accept sales pitches masked as presentations</li>
                         <li className='speakers__principle'>We do not differentiate between speakers from partners and independent speakers</li>
                     </ul>
                     <P>
-                        If your talk adheres to these very important principles,
-                        you are more than welcome to submit it!
+                        If your talk adheres to these very important principles, you are more than welcome to submit it to our CfS!
                     </P>
                 </Content>
             </Block>
