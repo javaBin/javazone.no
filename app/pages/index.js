389663bb1f538b3d26f701d5e2ed05cfcb2bb109
Bendik Solheim <bendik@solheim.im>
2017-01-31 19:58:41 +0100

    New fonts. Fixed logo. Start on new design

--- app/pages/index.js
+++ app/pages/index.js
@@ -4,18 +4,12 @@ import { Page } from '../components/page';
 import { TransitionWord } from '../components/word-transition.js';
 import { Link } from '../components/link';
 
-const twoSettings = {minWrongChars: 20, maxWrongChars: 25, delay: 900};
-const fourSettings = {minWrongChars: 25, maxWrongChars: 35, delay: 900};
-const textSettings = {minWrongChars: 10, maxWrongChars: 20, delay: 900};
-
 const Index = () => (
     <Page name='index' showLogo={false}>
         <div className='index__content'>
             <img className='index__logo' src={logo} />
             <div className='index__text'>
-                <TransitionWord settings={textSettings}>September 13th-14th 2017</TransitionWord>
-                <TransitionWord settings={textSettings}>Oslo / Norway / World</TransitionWord>
-                <TransitionWord settings={textSettings}>Save the date</TransitionWord>
+                Welcome to<br />JavaZone 2017
             </div>
             <p className='index__teasertext'>While you wait:</p>
             <ul className='index__links'>
