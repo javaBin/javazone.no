c30cfbcd20e34dcbe8eac58725f5c55d2c4898dc
Rustam Mehmandarov <mehmandarov@users.noreply.github.com>
2017-02-01 21:12:32 +0100

    Making eslint happy. Fixing spaces and tabs.

--- app/pages/speakers.js
+++ app/pages/speakers.js
@@ -53,13 +53,13 @@ const Speakers = () => (
                         JavaZone 2017 will be the 2^4th consecutive JavaZone conference, and will consist of a day of workshops followed by two days of presentations and (more) workshops. Last year's event was a great success with more than 3000 attendees enjoying 173 talks covering a wide range of topics. JavaZone speakers include both internationally renowned speakers and home-grown Norwegian talent.
                     </P>
                     <P>
-                    	Over the last decade JavaZone has grown to become one of the most important Java events in Europe! You can see for yourself – videos from previous years are available for free at <a href='https://vimeo.com/javazone'>Vimeo</a>, and the full program from JavaZone 2016 can be found on <a href='https://2016.javazone.no/program'>last year’s web page</a>.
+                        Over the last decade JavaZone has grown to become one of the most important Java events in Europe! You can see for yourself – videos from previous years are available for free at <a href='https://vimeo.com/javazone'>Vimeo</a>, and the full program from JavaZone 2016 can be found on <a href='https://2016.javazone.no/program'>last year’s web page</a>.
                     </P>
                     <P>
                         We can brag as much as we want, but JavaZone would be nothing without all the great speakers! And that is why we need your help to make sure that JavaZone 2017 will be at least as awesome as the earlier events. If you think you have something interesting to share, please submit your talk or workshop by the 24th of April, 2017.
                     </P>
                     <P>
-                    	JavaZone is committed to diversity, and we are especially interested in quality submissions from groups traditionally under-represented in tech.
+                        JavaZone is committed to diversity, and we are especially interested in quality submissions from groups traditionally under-represented in tech.
                     </P>
                 </CContent>
             </CBlock>
@@ -107,20 +107,20 @@ const Speakers = () => (
                         Talks do not have to be about JVM technology. We will consider all talks based on their relevance to developers and technical architects.
                     </P>
                     <P>
-                    	We are always interested in Java technology and its applications, current trends in programming, and experience reports. Topics we would like to hear more about this year include:
+                        We are always interested in Java technology and its applications, current trends in programming, and experience reports. Topics we would like to hear more about this year include:
                     </P>
                     <ul>
-                	    <li>Front-end patterns and architectures.</li>
-					    <li>Practical functional programming.</li>
-					    <li>Machine learning, AI, deep learning.</li>
-					    <li>Hacking, security, cryptography, blockchains.</li>
-                	</ul>
-                	<P>
-                		... and, of course, your killer topic.
-                	</P>
-                	<P>
-                		When you submit your talk, you should indicate whether there will be code in your slides or not. If you are going to perform live coding, please state that as well!
-                	</P>
+                        <li>Front-end patterns and architectures.</li>
+                        <li>Practical functional programming.</li>
+                        <li>Machine learning, AI, deep learning.</li>
+                        <li>Hacking, security, cryptography, blockchains.</li>
+                    </ul>
+                    <P>
+                        ... and, of course, your killer topic.
+                    </P>
+                    <P>
+                        When you submit your talk, you should indicate whether there will be code in your slides or not. If you are going to perform live coding, please state that as well!
+                    </P>
                     <SubHeader>Get Some Inspiration</SubHeader>
                     <dl className='speakers__inspiration'>
                         <dt className='speakers__inspiration-title'>Speakers workshop</dt>
@@ -135,17 +135,17 @@ const Speakers = () => (
                 <CHeader>Dates & Deadlines</CHeader>
                 <CContent>
                 <P>
-	                Call for speakers closes April 27th. Speakers will be notified if their proposal is accepted at the latest June 26th. JavaZone will be held in Oslo September 13th-14th. Workshops will be Septhember 12th.
+                    Call for speakers closes April 27th. Speakers will be notified if their proposal is accepted at the latest June 26th. JavaZone will be held in Oslo September 13th-14th. Workshops will be Septhember 12th.
                 </P>
-				<P>
-					This year, we are itching to try out a new feature in our submission system: Interactive feedback in the CfP phase. In short, early submissions may be considered as they arrive, and potentially receive feedback from the program committee. This includes feedback on points we find interesting, suggestions for changes and additions etc.
-				</P>
-				<P>
-					We also have a selfish motive. At JavaZone, we receive a huge number of submissions every year, and the workload faced by the program committee at the end of the CfP phase can be overwhelming. By encouraging early submissions, we hope to get ahead of the game this year.
-				</P>
-				<P>
-					Bottom line: The earlier you submit, the more likely you are to be noticed, and to receive considered and helpful feedback. Avoid drowning in the end-of-CfP tsunami! In addition, submissions received by March 27 will be in the draw for a special treat!
-				</P>
+                <P>
+                    This year, we are itching to try out a new feature in our submission system: Interactive feedback in the CfP phase. In short, early submissions may be considered as they arrive, and potentially receive feedback from the program committee. This includes feedback on points we find interesting, suggestions for changes and additions etc.
+                </P>
+                <P>
+                    We also have a selfish motive. At JavaZone, we receive a huge number of submissions every year, and the workload faced by the program committee at the end of the CfP phase can be overwhelming. By encouraging early submissions, we hope to get ahead of the game this year.
+                </P>
+                <P>
+                    Bottom line: The earlier you submit, the more likely you are to be noticed, and to receive considered and helpful feedback. Avoid drowning in the end-of-CfP tsunami! In addition, submissions received by March 27 will be in the draw for a special treat!
+                </P>
 				</CContent>
                 <CContent>
                     <Timeline />
