89391d18a20e79536a519ce6b6ea2cda2f037986
Bendik Solheim <bendik@solheim.im>
2017-02-09 22:49:23 +0100

    Will the icon work now?

--- tasks/failed.sh
+++ tasks/failed.sh
@@ -5,9 +5,9 @@ set -e
 echo "Build broken, notifying on slack"
 
 if [[ $TRAVIS_BRANCH == "master" ]]; then
-    curl -X POST --data-urlencode 'payload={"channel": "#javazone-web", "username": "[PROD] Deploy-bot", "text": "Bygget brakk! Se hvorfor på https://travis-ci.org/javaBin/javazone.no", "icon_emoji": "face_with_head_bandage"}' ${slackuri} > /dev/null
+    curl -X POST --data-urlencode 'payload={"channel": "#javazone-web", "username": "[PROD] Deploy-bot", "text": "Bygget brakk! Se hvorfor på https://travis-ci.org/javaBin/javazone.no", "icon_emoji": ":face_with_head_bandage:"}' ${slackuri} > /dev/null
 else
-    curl -X POST --data-urlencode 'payload={"channel": "#javazone-web", "username": "[TEST] Deploy-bot", "text": "Bygget brakk! Se hvorfor på https://travis-ci.org/javaBin/javazone.no", "icon_emoji": "face_with_head_bandage"}' ${slackuri} > /dev/null
+    curl -X POST --data-urlencode 'payload={"channel": "#javazone-web", "username": "[TEST] Deploy-bot", "text": "Bygget brakk! Se hvorfor på https://travis-ci.org/javaBin/javazone.no", "icon_emoji": ":face_with_head_bandage:"}' ${slackuri} > /dev/null
 fi;
 
 echo "Slack notified"
