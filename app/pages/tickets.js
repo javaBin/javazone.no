7fd129186196ed50069a65a91223540973ff5ffc
Mark West <markwest1972@users.noreply.github.com>
2017-02-09 20:24:21 +0100

    Small ch-ch-ch-changes.

--- app/pages/tickets.js
+++ app/pages/tickets.js
@@ -24,8 +24,8 @@ const Tickets = () => (
                 <CContent>
                     <div className="partners__mainprice">NOK 5.890,-</div>
                     <P>
-                        Get your JavaZone ticket now. It includes full access to the conference, <br />
-                        including access to workshops on Tuesday (limited seating: first come, first served).<br />
+                        Get your JavaZone ticket now. It includes full access to the conference <br />
+                        and entry to workshops on Tuesday (limited seating: first come, first served).<br />
                         The price also includes a 1 year membership in javaBin, the Norwegian Java User Group<br />
                         <em className="partners__latebirdprice">After April 1st, the Early Bird ends and the ticket price increases to 6.490,-</em><br />
                         <span className="partners__vat">prices ex VAT</span>
@@ -40,7 +40,7 @@ const Tickets = () => (
                 <Header><span className='blue'>Working for one of our partners?</span></Header>
                 <Content>
                     <P>
-                        All partners of JavaZone get reduced prices on tickets. Partners can buy tickets at reduced prices until July 1st. Read more about <Link href='/partners'>partnerships of JavaZone</Link> and contact us at <a href="mailto:partner@java.no">partner@java.no</a> to buy tickets.
+                        All partners of JavaZone get reduced prices on tickets. Partners can buy tickets at a reduced price until July 1st. Read more about <Link href='/partners'>JavaZone partnership</Link> and contact us at <a href="mailto:partner@java.no">partner@java.no</a> to buy tickets.
                     </P>
                 </Content>
             </Block>
@@ -49,7 +49,7 @@ const Tickets = () => (
                 <Header><span className='pink'>Need more than one ticket?</span></Header>
                 <Content>
                     <P>
-                        Multiple tickets can be bought at the same time. You can then assign the tickets to the participants via email. Just use our regular webshop and specify the number of tickets you need when buying.
+                        Multiple tickets can be bought at the same time. You can then assign the tickets to the participants via email. Just use our regular webshop and specify the number of tickets you require when buying.
                     </P>
                 </Content>
             </Block>
@@ -58,7 +58,7 @@ const Tickets = () => (
                 <Header><span className='orange'>Need an invoice?</span></Header>
                 <Content>
                     <P>
-                        Paying by invoice can be accommodated if needed, but please consider buying the tickets using the webshop if you can. You’ll get a receipt which can be used for reimbursements even though paying with credit card.
+                        Paying by invoice can be accommodated if required, but please consider buying the tickets using the webshop if you can. You’ll get a receipt which can be used for reimbursements even when paying with credit card.
                         <br />
                         Request your invoice here: <a href="https://goo.gl/forms/1vJi5DrpBO279Gyl2">Invoice request form</a>.
                     </P>
@@ -84,7 +84,7 @@ const Tickets = () => (
                 <CContent>
                     <P>
                         <br />
-                        JavaZone is organized for and by the community. Your ticket includes a membership in javaBin, giving you access to all the javaBin community events and voting rights for a full year after buying the ticket.
+                        JavaZone is organized for and by the community. Your ticket includes a membership to javaBin, giving you access to all the javaBin community events and voting rights for a full year after buying the ticket.
                     </P>
                     <P>
                         Previously we used to sell tickets and memberships separatly, but you saved a total of 500 NOK by buying both. It seemed silly to force you through two separate web shops to collect this discount, so we combined the two and thereby made the tickets cheaper for everyone. If you still would like to pay extra as before, let us know! ;-)
@@ -99,10 +99,10 @@ const Tickets = () => (
                 <CContent>
                     <P>
                         <br />
-                        We'll release more information about the conference before the summer, and the detailed program with information about all the talks will be ready in July. Until then, have a look at the video on top and <a href="http://2016.javazone.no">the 2016 webpages</a> to get a feel of how awesome JavaZone is!
+                        We'll release more information about the conference before the summer, and the detailed program with information about all the talks will be ready in July. Until then, have a look at the video on top and <a href="http://2016.javazone.no">the 2016 website</a> to get a feel for how awesome JavaZone is!
                     </P>
                     <P>
-                        Don't wait too long buying your ticket though – last year we completely sold out all the 3.000 tickets, and we expect a decent amout of interest this year as well.
+                        Don't wait too long buying your ticket though – last year we completely sold out all of the 3.000 tickets, and we expect a decent amout of interest this year as well.
                     </P>
                 </CContent>
             </CBlock>
