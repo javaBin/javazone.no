742e05c394db07f8db898779b0cd732ce0d11178
Gareth Western <gareth@garethwestern.com>
2017-07-17 12:40:07 +0200

    Oppdatert datoene og session detaljer

--- app/pages/kids.js
+++ app/pages/kids.js
@@ -4,37 +4,30 @@ import { getWorkshops } from '../actions/workshops';
 import { Page, Heading, LargeHeading, SmallHeading, Container, Pitch } from '../components/page';
 import { Block, Header, Content, SubHeader, P} from '../components/block';
 import { CBlock, CHeader, CContent } from '../components/centeredblock';
-import mbot from '../assets/kids/mbot.jpg';
-import codestudio from '../assets/kids/codestudio.jpg';
-import minecraft from '../assets/kids/minecraft.jpg';
-import arduino from '../assets/kids/arduino.jpg';
+import sonicpi from '../assets/kids/sonicpi.jpg';
+import scratch from '../assets/kids/scratch.jpg';
+import appinventor from '../assets/kids/appinventor.jpg';
 import { find } from 'lodash/fp';
 import kidsimage from '../assets/kids.png';
 
 const kidsInfo = {
-    'kids_codestudio': {
-        background: codestudio,
-        age: '4-10 år',
-        title: 'Codestudio',
+    'kids_sonicpi': {
+        background: sonicpi,
+        age: '13+ år',
+        title: 'Sonic Pi med Sam Aaron',
         odd: true
     },
-    'kids_computercraft': {
-        background: minecraft,
+    'kids_scratch': {
+        background: scratch,
         age: '11+ år',
-        title: 'ComputerCraft i Minecraft',
+        title: 'Scratch',
         odd: false
     },
-    'kids_robot': {
-        background: mbot,
-        age: '10-14år',
-        title: 'Lær å programmere roboter',
-        odd: true
-    },
-    'kids_arduino': {
-        background: arduino,
+    'kids_appinventor': {
+        background: appinventor,
         age: '12+ år',
-        title: 'Kreativ Elektronikk med Arduino',
-        odd: false
+        title: 'App Inventor',
+        odd: true
     }
 };
 
@@ -62,7 +55,7 @@ function workshopClass(workshop) {
 
 function workshopStatus(workshop) {
     if (!workshop) {
-        return 'Påmelding 20. februar kl. 12.00';
+        return 'Påmelding 21. august kl. 12.00';
     }
 
     switch (workshop.status) {
@@ -71,7 +64,7 @@ function workshopStatus(workshop) {
     case 'FULL': return 'Påmelding m/ venteliste';
     case 'VERY_FULL': return 'Ingen ledige plasser';
     case 'CLOSED': return 'Påmelding stengt';
-    default: return 'Påmelding 20. februar kl. 12.00';
+    default: return 'Påmelding 21. august kl. 12.00';
     }
 }
 
@@ -113,15 +106,14 @@ const Session = ({session, children}) => {
 };
 
 const Kids = ({workshops}) => {
-    const wcodestudio = find({id: 'kids_codestudio'}, workshops);
-    const wminecraft = find({id: 'kids_computercraft'}, workshops);
-    const wrobot = find({id: 'kids_robot'}, workshops);
-    const warduino = find({id: 'kids_arduino'}, workshops);
+    const wsonicpi = find({id: 'kids_sonicpi'}, workshops);
+    const wscratch = find({id: 'kids_scratch'}, workshops);
+    const wappinventor = find({id: 'kids_appinventor'}, workshops);
     return (
         <Page name='kids'>
             <Heading>
                 <LargeHeading>JavaZone Kids</LargeHeading>
-                <SmallHeading>19. mars 2017</SmallHeading>
+                <SmallHeading>10. sept 2017</SmallHeading>
             </Heading>
 
             <img className='kids__largeimage' src={kidsimage} />
@@ -134,7 +126,7 @@ const Kids = ({workshops}) => {
                             <br />
                             JavaZone inviterer alle, og spesielt deltakere på JavaZone,
                             til å ta med seg sine barn på Teknologihuset
-                            søndag 19. mars for å gi dem en smakebit på
+                            søndag 10. september for å gi dem en smakebit på
                             voksenlivet. Vi kjører 4 parallelle sesjoner,
                             slik at vi har noe for både de aller minste
                             barna og de største ungdommene.
@@ -157,7 +149,7 @@ const Kids = ({workshops}) => {
                         <SubHeader>Tid & Sted</SubHeader>
                         <P>
                             Arrangementet finner sted på Teknologihuset
-                            søndag 19. mars fra klokken 12.00 til
+                            søndag 10. september fra klokken 12.00 til
                             15.00. Vi serverer enkel mat underveis. Det
                             er viktig at du som forelder også setter av
                             tid til å være til stede hele perioden. Vi
@@ -167,7 +159,7 @@ const Kids = ({workshops}) => {
                         <P>
                             Vi har et begrenset antall plasser, derfor er det viktig at
                             du melder dine barn på arrangementet.
-                            Påmeldingen åpner mandag 20. februar kl. 12.00.
+                            Påmeldingen åpner mandag 21. august kl. 12.00.
                         </P>
                         <SubHeader>Forberedelser</SubHeader>
                         <P>
@@ -179,10 +171,10 @@ const Kids = ({workshops}) => {
                 </Block>
 
                 <Block>
-                    <Header><span className="orange">4 Parallelle Sesjoner</span></Header>
+                    <Header><span className="orange">3 Parallelle Sesjoner</span></Header>
                     <Content>
                         <P>
-                            Vi kjører 4 parallelle sesjoner. Du kan lese mer om
+                            Vi kjører 3 parallelle sesjoner. Du kan lese mer om
                             de ulike sesjonene under, og melde dere på den dere
                             ønsker å delta på. Har du flere barn i forskjellige
                             aldere som ønsker å være med på ulike sesjoner, så
@@ -190,74 +182,48 @@ const Kids = ({workshops}) => {
                             for å melde på riktig antall barn til hver sesjon.
                         </P>
                         <P>
-                            Påmeldingen åpner mandag 20. februar kl. 12.00.
+                            Påmeldingen åpner mandag 21. august kl. 12.00.
                         </P>
                     </Content>
                 </Block>
 
                 <div className="kids__separator"></div>
 
-                <Session session={wcodestudio}>
+                <Session session={wsonicpi}>
                     <P>
-                        Programmering med CodeStudio sine oppgaver for de aller
-                        yngste. Her vil vi ta i bruk <a href="https://code.org">Code.org</a>
-                        sine opplegg for Kodetimen. Dette vil bli en lett
-                        introduksjon som verken krever forkunnskaper i lesing
-                        eller matematikk.
+                       TODO
                     </P>
                     <P>
-                        Oppgavene finnes på <a href="https://studio.code.org">studio.code.org</a>.
+                        Sonic Pi programvære finnes på <a href="http://sonic-pi.net/">sonic-pi.net</a>.
                     </P>
                 </Session>
 
-                <Session session={wminecraft}>
+                <Session session={wscratch}>
                     <P>
-                        ComputerCraft er en mod til Minecraft, som lar deg
-                        bygge datamaskiner og roboter inne i spillet.
-                        Disse datamaskinene og robotene kan programmeres til
-                        å kontrollere dører, grave huler, bygge hus og så videre.
-                        ComputerCraft bruker programmeringsspråket Lua, som er
-                        et enkelt og fleksibelt tekstbasert programmeringsspråk.
-                        For å delta på dette kurset må deltagerne ha en
-                        Minecraft-konto.
+                        TODO Scratch
                     </P>
                     <P>
-                        <a href="http://kodeklubben.github.io/computercraft/installasjon/installasjon.html">Oppskrift for hvordan du installerer ComputerCraft</a>.
+                        TODO
                     </P>
                 </Session>
 
-                <Session session={wrobot}>
+                <Session session={wappinventor}>
                     <P>
-                        Bygg og styr en <a href="http://makeblock.com/mbot-stem-educational-robot-kit-for-kids/">mBot</a> både med en app eller la den styre seg selv. Vi får roboten til å bruke motor, sensorer, lys og lyd mens vi lærer hvordan man kan styre den.
+                        TODO App Inventor
                     </P>
                     <P>
-                        Se også <a href="https://www.youtube.com/watch?v=pmsSipper3Y">videoen som forklarer mBot</a>.
-                    </P>
-                </Session>
-
-                <Session session={warduino}>
-                    <P>
-                        Ved å lære enkel programmering lærer man å bli kreativ med et
-                        av de kraftigste arbeidsverktøyene vi har i dag.
-                    </P>
-                    <P>
-                        Arduino gjør det mulig å styre alt fra en LED-dimmer
-                        til en 3D-printer. I dette tretimerskurset skal vi
-                        lære mer om hva en Arduino er og hva elektronikk kan
-                        brukes til. Men aller viktigst - vi skal lære å få lamper
-                        til å blinke, vi skal måle temperaturen i rommet -
-                        og kanskje vi rekker å få en motor til å rotere!
+                        TODO
                     </P>
                 </Session>
 
                 <CBlock>
                     <CHeader>
-                        Vi sees på Teknologihuset søndag 19. mars!
+                        Vi sees på Teknologihuset søndag 10. september!
                     </CHeader>
                     <CContent>
                         <P>
                             Vennlig hilsen,<br />
-                            javaBin, Lær Kidsa Koding, Bitraf og Kodegenet
+                            javaBin og Lær Kidsa Koding
                         </P>
                     </CContent>
                 </CBlock>
