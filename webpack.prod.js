6b26a8e48d91fc2ed0c919a0b2c4cf0fd6f64300
Rustam Mehmandarov <mehmandarov@gmail.com>
2017-11-08 00:43:45 +0100

    Fix webpack config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
-const commonConfig = './webpack.common.js';
+const commonConfig = require('./webpack.common.js');
 
 module.exports = webpackMerge(commonConfig, {
     plugins: [
