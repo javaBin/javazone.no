77e4625da16a1a50f2d99a96dc2783d98129db1c
Mark West <markwest1972@users.noreply.github.com>
2018-01-20 16:56:40 +0100

    Revert "Update for CfS"

--- app/routes.js
+++ app/routes.js
@@ -67,9 +67,6 @@ const routes = compile({
     '/academy/tromso': academyLocation(academyData.tromso),
     '/academy/bergen': academyLocation(academyData.bergen),
     '/info': info,
-    '/speakers': speakers,
-    '/speakers/monetary-policy': monetaryPolicy,
-    '/speakers/tips': tipsAndTricks,
     '/': index
 });
 
