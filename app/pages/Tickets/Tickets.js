eba45c7dc4ad2a700c51abdf424a125d8d21f5e4
Rustam Mehmandarov <mehmandarov@gmail.com>
2018-02-11 23:59:29 +0100

    Revamped tickets page. Now looking shiny.

--- app/pages/Tickets/Tickets.js
+++ app/pages/Tickets/Tickets.js
@@ -1,12 +1,16 @@
 //@flow
 import * as React from 'react';
 import { Heading, LargeHeading, SmallHeading, Container, Pitch } from '../../components/page';
-import Page from '../../components/Page/Page';
-import { Section } from '../../components/Section/Section';
-import PageHeader from '../../components/PageHeader/PageHeader';
-import { Block, Header, Content, SubHeader, P} from '../../components/block';
+import { Block, Content, SubHeader, P} from '../../components/block';
 import { CBlock, CHeader, CContent } from '../../components/centeredblock';
 import { Link } from '../../components/link';
+import { Section } from '../../components/Section/Section';
+import { CenterBlock, LeftBlock, ImageBlock } from '../../components/Block/Block';
+import { Header } from '../../components/Header/Header';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import Button from '../../components/Button/Button';
+import Page from '../../components/Page/Page';
+import PageHeader from '../../components/PageHeader/PageHeader';
 import Timeline from '../../components/timeline';
 import Youtube from '../../components/youtube';
 import tickets1 from '../../assets/tickets_1.jpg';
@@ -20,54 +24,38 @@ type TicketsProps = {
 function Tickets(props: TicketsProps) {
     return (
             <Page name='tickets'>
-        <Heading>
-            <LargeHeading>Tickets to JavaZone 2018</LargeHeading>
-            <SmallHeading>Come join us in Oslo, September 12-13th</SmallHeading>
-        </Heading>
-        <Container>
+	        <PageHeader subHeader="Come join us in Oslo, September 12-13th">Tickets to JavaZone 2018</PageHeader>
+	        
+	        <Section>
+	            <CenterBlock header="Get your tickets for JavaZone 2018">
+	                    <div className="partners__mainprice">NOK 5,990</div>
+	                    <P>
+	                        The JavaZone Ticket includes full access to the conference <br />
+	                        and entry to workshops on Tuesday (limited seating: first come, first served).<br />
+	                        The price also includes a 1 year membership in javaBin, the Norwegian Java User Group<br />
+	                        <span className="partners__vat">prices ex VAT</span>
+	                    </P>
+	                    <P>
+	                        <a className='button button--transparent' href="https://www.eventbrite.com/e/javazone-2018-tickets-43071469926">Get Your Tickets Now!</a>
+	                    </P>
+	            </CenterBlock>	            
+	            <ImageBlock image={tickets3} alt="Tickets page separator image" />
+	        </Section>
 
-            <CBlock fullWidth={true} className='partners__price-container'>
-                <CHeader><span className='pink'>Get your tickets for JavaZone 2018</span></CHeader>
-                <CContent>
-                    <div className="partners__mainprice">NOK 5,990</div>
-                    <P>
-                        The JavaZone Ticket includes full access to the conference <br />
-                        and entry to workshops on Tuesday (limited seating: first come, first served).<br />
-                        The price also includes a 1 year membership in javaBin, the Norwegian Java User Group<br />
-                        <span className="partners__vat">prices ex VAT</span>
-                    </P>
-                    <P>
-                        <a className='button button--transparent' href="https://www.eventbrite.com/e/javazone-2018-tickets-43071469926">Get Your Tickets Now!</a>
-                    </P>
-                </CContent>
-            </CBlock>
-        </Container>
-
-        <img className='partners__image' src={tickets3} />
-
-        <Container>
-
-            <Block>
-                <Header><span className='blue'>Working for one of our partners?</span></Header>
-                <Content>
+	        <Section>
+	        	<LeftBlock header="Working for one of our partners?">
                     <P>
                          All partners of JavaZone get reduced prices on tickets. Partners can buy tickets at a reduced price until July 1st. The main partner contact person can order tickets by using this <a href="https://goo.gl/forms/mXtFNeaO9d73Mxwq1">invoice request form</a>. Read more about <Link href='/partners'>JavaZone partnership</Link> here.
                     </P>
-                </Content>
-            </Block>
+	            </LeftBlock>
 
-            <Block>
-                <Header><span className='pink'>Need more than one ticket?</span></Header>
-                <Content>
+	            <LeftBlock header="Need more than one ticket?">
                     <P>
                         Multiple tickets can be bought at the same time. You can then assign the tickets to the participants via email. Just use our regular webshop and specify the number of tickets you require when buying.
                     </P>
-                </Content>
-            </Block>
+	            </LeftBlock>
 
-            <Block>
-                <Header><span className='orange'>Need an invoice?</span></Header>
-                <Content>
+	            <LeftBlock header="Need an invoice?">
                     <P>
                         Paying by invoice can be accommodated if required, but please consider buying the tickets using the webshop if you can. You’ll get a receipt which can be used for reimbursements even when paying with credit card.
                         <br />
@@ -75,26 +63,20 @@ function Tickets(props: TicketsProps) {
                         <br />
                         Tickets are sold out...
                     </P>
-                </Content>
-            </Block>
+	            </LeftBlock>
 
-            <Block>
-                <Header><span className='green'>Refund policy</span></Header>
-                <Content>
+	            <LeftBlock header="Refund policy">
                     <P>
                         If you need a refund on your ticket, send an email with your request to <a href="mailto:javazone@java.no">javazone@java.no</a>.
                         We'll refund the full ticket price minus a small processing fee (to cover credit card fees or invoice fees) for requests submitted before August 1st.
                         Refund requests received after August 1st will only be accepted if javaBin are able to resell the tickets.
                         For tickets bought in bulk (applies to both partner tickets and large regular orders), a maximum of 5 tickets can be refunded under this policy.
                     </P>
-                </Content>
-            </Block>
+	            </LeftBlock>
 
-            <img className='partners__image' src={tickets1} />
+            	<ImageBlock image={tickets1} alt="Tickets page separator image" />
 
-            <CBlock>
-                <CHeader><span className='tickets__news green'>New this year:</span>The javaBin membership included in the ticket</CHeader>
-                <CContent>
+	            <LeftBlock header="New this year: The javaBin membership included in the ticket">
                     <P>
                         <br />
                         JavaZone is organized for and by the community. Your ticket includes a membership to javaBin, giving you access to all the javaBin community events and voting rights for a full year after buying the ticket.
@@ -102,25 +84,21 @@ function Tickets(props: TicketsProps) {
                     <P>
                         Previously we used to sell tickets and memberships separately, but you saved a total of 500 NOK by buying both. It seemed silly to force you through two separate web shops to collect this discount, so we combined the two and thereby made the tickets cheaper for everyone. If you still would like to pay extra as before, let us know! ;-)
                     </P>
-                </CContent>
-            </CBlock>
+	            </LeftBlock>
 
-            <img className='partners__image' src={tickets2} />
+            	<ImageBlock image={tickets2} alt="Tickets page separator image" />
 
-            <CBlock>
-                <CHeader><span className='blue'>Need more info about JavaZone?</span></CHeader>
-                <CContent>
+	            <CenterBlock header="Need more info about JavaZone?">
                     <P>
                         <br />
-                        We'll release more information about the conference before the summer, and the detailed program with information about all the talks will be ready in July. Until then, have a look at the video on top and <a href="http://2016.javazone.no">the 2016 website</a> to get a feel for how awesome JavaZone is!
+                        We'll release more information about the conference before the summer, and the detailed program with information about all the talks will be ready in July. Until then, have a look at the video on top and <a href="http://2017.javazone.no">the 2017 website</a> to get a feel for how awesome JavaZone is!
                     </P>
                     <P>
-                        Don't wait too long buying your ticket though – last year we completely sold out all of the 3.000 tickets, and we expect a decent amout of interest this year as well.
+                        Don't wait too long buying your ticket though – last year we all of the 3,100 spots were gone, and we had long waiting lists. We expect a decent amout of interest this year as well.
                     </P>
-                </CContent>
-            </CBlock>
-        </Container>
-    </Page>
+	            </CenterBlock>
+	        </Section>
+	    </Page>
     )
 }
 
