c6979d4f9b64f0c5ca654b94e49fe4889ca38555
Rustam Mehmandarov <mehmandarov@gmail.com>
2018-02-18 00:15:48 +0100

    Added formatting to 404 page

--- app/pages/404.js
+++ app/pages/404.js
@@ -19,11 +19,17 @@ function Page404(props: Page404Props) {
                     <p>
                         We seem to somehow have lost the page you are looking for.
                         If you are a technical person, this is what you might usually
-                        refer to as a classical 404 response code. We are of course
+                        refer to as a classical 404 response code. 
+                    </p>
+                    <p>
+                        We are of course
                         using advanced technology to keep things like these from
-                        happening, but every now and then technology (or we...) fails. If you
-                        have a lot of time on your hands (or are just really kind),
-                        we would really appreciate it if you <a href='mailto:javazone@java.no'>tell us about it</a>.
+                        happening, but every now and then technology (or we...) fails. 
+                    </p>
+                    <p>
+                        If you have a lot of time on your hands (or are just really kind),
+                        we would really appreciate it if you <a href='mailto:javazone@java.no'>
+                        tell us about it</a>.
                     </p>
                 </LeftBlock>
             </Section>
