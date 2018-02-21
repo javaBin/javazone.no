9a93ecd60620a843c798f105e601a1f5800a0ffb
sl <sl@computas.com>
2018-02-21 22:14:40 +0100

    Adjusted plane and bus delay a bit

--- app/components/Banner/Banner.js
+++ app/components/Banner/Banner.js
@@ -1,6 +1,5 @@
 //@flow
 import * as React from 'react';
-import classnames from 'classnames';
 import './Banner.less';
 
 // SVGs
@@ -17,45 +16,34 @@ import cloud2 from '../../assets/2018/banner/cloud2.svg';
 import cloud3 from '../../assets/2018/banner/cloud3.svg';
 import cloud4 from '../../assets/2018/banner/cloud4.svg';
 import cloud5 from '../../assets/2018/banner/cloud5.svg';
+import cloud6 from '../../assets/2018/banner/cloud6.svg';
+import cloud7 from '../../assets/2018/banner/cloud7.svg';
+import cloud8 from '../../assets/2018/banner/cloud8.svg';
 
-type BannerProps = {
-}
-
-type BannerState = {
-    bannerHeight: number
-}
-
-class Banner extends React.Component<BannerProps, BannerState> {
-    render() {
-
-        let bannerClass = classnames({
-            'banner': true
-        })
-
-        return (
-            <div className={bannerClass}>
-                <img className="background" src={background} />
-                <img className="fog-layer" src={fog} />
-                <img className="city-layer" src={cityBack} />
-                <img className="city-layer" src={cityFront} />
-                <img className="tree-layer" src={trees} />
-                <img className="bus-layer" src={bus} />
-                <img className="cloud1-layer" src={cloud1} />
-                <img className="cloud2-layer" src={cloud2} />
-                <img className="cloud3-layer" src={cloud3} />
-                <img className="cloud4-layer" src={cloud4} />
-                <img className="cloud5-layer" src={cloud5} />
-                <img className="cloud6-layer" src={cloud4} />
-                <img className="cloud7-layer" src={cloud3} />
-                <img className="cloud8-layer" src={cloud2} />
-                <img className="plane-layer" src={plane} />
-                <img className="logo-layer" src={javazone2018Logo} />
-                <div className="logo-text">
-                    <h1>JAVAZONE 2018</h1>
-                </div>
+function Banner() {
+    return (
+        <div className="banner">
+            <img className="background" src={background} />
+            <img className="fog-layer" src={fog} />
+            <img className="city-layer" src={cityBack} />
+            <img className="city-layer" src={cityFront} />
+            <img className="tree-layer" src={trees} />
+            <img className="bus-layer" src={bus} />
+            <img className="cloud1-layer" src={cloud1} />
+            <img className="cloud2-layer" src={cloud2} />
+            <img className="cloud3-layer" src={cloud3} />
+            <img className="cloud4-layer" src={cloud4} />
+            <img className="cloud5-layer" src={cloud5} />
+            <img className="cloud6-layer" src={cloud6} />
+            <img className="cloud7-layer" src={cloud7} />
+            <img className="cloud8-layer" src={cloud8} />
+            <img className="plane-layer" src={plane} />
+            <img className="logo-layer" src={javazone2018Logo} />
+            <div className="logo-text">
+                <h1>JAVAZONE 2018</h1>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Banner;
