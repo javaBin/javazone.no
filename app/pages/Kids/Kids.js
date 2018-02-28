d5180870a47c2c364d1386bafd04ec7219adaaef
Rustam Mehmandarov <mehmandarov@gmail.com>
2018-03-01 00:12:57 +0100

    Typo fixin'

--- app/pages/Kids/Kids.js
+++ app/pages/Kids/Kids.js
@@ -24,11 +24,11 @@ const kidsInfo = {
         description: 'Hils på Tom! Tom er en skilpadde. ' +
         'Tom lærer raskt, og er god på å følge instrukser. ' +
         'Han er dyktig med farger og bevegelse, og gjør stort sett alt du ber ham om. ' +
-        'I løpet av 2 timer lære vi først å kommandere Tom. Vi får ham til å bevege seg og tegne det vi vil. ' +
-        'Så lærer vi ham nye ord og kommandoer.  Og ja; han lærer gjerne ord på hvilket som helst språk. ' +
+        'I løpet av 2 timer lærer vi først å kommandere Tom. Vi får ham til å bevege seg og tegne det vi vil. ' +
+        'Så lærer vi ham nye ord og kommandoer. Og ja; han lærer gjerne ord på hvilket som helst språk. ' +
         'Vi kommer også til å lære hvordan man styrer flere skilpadder samtidig. Og har vi tid, så bygger vi ' +
         'til og med et spill. ' +
-        'Deretter tar du med deg det du har lært hjem - for å fortsette å utvikle dine kunnskaper og ferdigheter. ' +
+        'Deretter tar du med deg det du har lært hjem – for å fortsette å utvikle dine kunnskaper og ferdigheter. ' +
         'Og du må gjerne dele med venner og familie, på skolen, og online.',
         url: 'https://www.george.andante.no/',
     },
