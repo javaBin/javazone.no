91343a8c7a00ae1fe9567dbda3b694c73322556c
Gareth Western <gareth@garethwestern.com>
2018-02-28 22:10:54 +0100

    Centered scratch photo

--- app/pages/Kids/Kids.js
+++ app/pages/Kids/Kids.js
@@ -11,7 +11,7 @@ import Button from '../../components/Button/Button';
 import Page from '../../components/Page/Page';
 import PageHeader from '../../components/PageHeader/PageHeader';
 import george from '../../assets/kids/george.png';
-import scratch from '../../assets/kids/scratch.jpg';
+import scratch from '../../assets/kids/scratch2.png';
 import minecraft from '../../assets/kids/minecraft.jpg';
 import kidsimage from '../../assets/kids/kids_header.jpg';
 import './Kids.less';
