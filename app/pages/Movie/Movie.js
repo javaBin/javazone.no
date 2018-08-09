26587cdd42b0d0e0b7c7bf3b0dca9cf64267ba24
sl <sl@computas.com>
2018-08-09 22:33:51 +0200

    Added countdown

--- app/pages/Movie/Movie.js
+++ app/pages/Movie/Movie.js
@@ -1,56 +1,43 @@
 import * as React from 'react';
 import Page from '../../components/Page/Page.js';
-import CurtainCountdown from '../../components/CurtainCountdown/CurtainCountdown.js';
-import * as moment from 'moment'; 
+import Countdown from 'react-countdown-now';
+import Youtube from '../../components/youtube';
+import moment from './time-zone-data.js'; 
 import './Movie.less';
 
-const END_DATE = '2018-08-08 22:00:00';
-
-type MovieProps = {
-}
-
-type MovieState = {
-    timeRemaining: string;
-}
+// Edit this deadline
+const END_DATE = '2018-08-13 10:00:00';
 
 class Movie extends React.Component<MovieProps, MovieState> {
 
     updateTime: Function;
-
-    state = {
-        timeRemaining: ''
-    }
 
     constructor(props: MovieProps) {
         super(props);
         this.updateTime = this.updateTime.bind(this);
     }
 
-    updateTime() {
-        const endDate = moment.Moment(END_DATE, 'YYYY-MM-DD HH:mm:ss');
-        const now = moment.Moment();
-        const seconds = endDate.diff(now);
-        const minutes = endDate.diff(now, 'minutes');
-        const hours = endDate.diff(now, 'hours');
-        const days = endDate.diff(now, 'days');
-        const lol = Moment(seconds).format('mm:ss');
-        console.log('lol', lol);
-        /*
-        this.setState({
-            timeRemaining: `${days} Days ${hours} ${hours === 1 ? 'Hour' : 'Hours'} ${minutes} ${minutes === 1 ? 'Minute' : 'Minutes'} ${seconds} ${seconds === 1 ? 'Second' : 'Seconds'}`
-        });
-        */
-    }
-
-    componentDidMount() {
-        setInterval(() => this.updateTime(), 1000);
+    updateTime({days, hours, minutes, seconds, completed}) {
+        const daysString = `${days} ${days === 1 ? 'day' : 'days'}`
+        const hoursString = `${hours} ${hours === 1 ? 'hour' : 'hours'}`
+        const minutesString = `${minutes} ${minutes === 1 ? 'minute' : 'minutes'}`
+        const secondsString = `${seconds} ${seconds === 1 ? 'second' : 'seconds'}`
+        const time = `${hoursString} ${minutesString} ${secondsString}`;
+        return (
+            <div className="movie-container">
+                {completed ? <Youtube id='dQw4w9WgXcQ'></Youtube>
+                    : <div className="movie-counter">
+                        {days !== 0 ? <h1>{daysString}</h1> : null}
+                        <h2>{time}</h2>
+                    </div>}
+            </div>
+        )
     }
 
     render() {
-        console.log('state', this.state);
         return (
             <Page name='movie'>
-                <CurtainCountdown timeRemaining={this.state.timeRemaining} />
+                <Countdown date={moment.tz(END_DATE, "Europe/Oslo").toDate()} zeroPadLength={0} renderer={this.updateTime} />
             </Page>
         );
     }
