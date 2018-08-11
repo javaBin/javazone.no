dba762b4ea7c8db151cb643f2be679376935f659
sl <sl@computas.com>
2018-08-11 17:30:30 +0200

    Added reload on countdown completed

--- app/pages/Movie/Movie.js
+++ app/pages/Movie/Movie.js
@@ -8,6 +8,8 @@ import './Movie.less';
 
 // Edit this deadline
 const END_DATE = '2018-08-13 10:00:00';
+// Edit this id
+const YOUTUBE_ID = 'dQw4w9WgXcQ';
 
 class Movie extends React.Component<MovieProps, MovieState> {
 
@@ -16,33 +18,49 @@ class Movie extends React.Component<MovieProps, MovieState> {
     constructor(props: MovieProps) {
         super(props);
         this.updateTime = this.updateTime.bind(this);
+        this.showMovie = this.showMovie.bind(this);
+    }
+
+    showMovie() {
+        return (
+            <div className="movie-container">
+                <Youtube id={YOUTUBE_ID}></Youtube>
+            </div>
+        );
+    }
+
+    renderWrapper() {
+
     }
 
     updateTime({hours, minutes, seconds, completed}) {
-        const hoursString = `${hours} ${hours === 1 ? 'hour' : 'hours'}`
-        const minutesString = `${minutes} ${minutes === 1 ? 'minute' : 'minutes'}`
-        const secondsString = `${seconds} ${seconds === 1 ? 'second' : 'seconds'}`
-        let time = `${hoursString} ${minutesString} ${secondsString}`;
-        if (hours === 0) {
-            time = `${minutesString} ${secondsString}`;
-            if (minutes === 0) {
-                time = `${secondsString}`;
+        if (!completed) {
+            const hoursString = `${hours} ${hours === 1 ? 'hour' : 'hours'}`
+            const minutesString = `${minutes} ${minutes === 1 ? 'minute' : 'minutes'}`
+            const secondsString = `${seconds} ${seconds === 1 ? 'second' : 'seconds'}`
+            let time = `${hoursString} ${minutesString} ${secondsString}`;
+            if (hours === 0) {
+                time = `${minutesString} ${secondsString}`;
+                if (minutes === 0) {
+                    time = `${secondsString}`;
+                }
             }
-        }
-        return (
-            <div className="movie-container">
-                {completed ? <Youtube id='dQw4w9WgXcQ'></Youtube>
-                    : <div className="movie-counter">
+            return (
+                <div className="movie-container">
+                    <div className="movie-counter">
                         <h2>{time}</h2>
-                    </div>}
-            </div>
-        )
+                    </div>
+                </div>
+            )
+        }
+        location.reload();
     }
 
     render() {
         return (
             <Page name='movie'>
-                <Countdown daysInHours date={moment.tz(END_DATE, "Europe/Oslo").toDate()} zeroPadLength={0} renderer={this.updateTime} />
+                {(moment.tz(END_DATE, 'Europe/Oslo').diff(moment()) <= 0) ? this.showMovie()
+                : <Countdown daysInHours date={moment.tz(END_DATE, "Europe/Oslo").toDate()} zeroPadLength={0} renderer={this.updateTime} />}
             </Page>
         );
     }
